6cffff15190475e6d5ac8a74874dd03d8899a5f3
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 01:33:43 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
+require 'customer.rb'
+require 'meal.rb'
+
 class Waiter
+
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end #initialize
+
+  def self.all
+    @@all
+  end #self.all
 
 end
