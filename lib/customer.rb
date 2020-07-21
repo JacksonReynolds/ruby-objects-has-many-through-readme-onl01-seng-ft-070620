04a47bbd9f31808ee91695e3c7bef53fc2a51d70
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 01:31:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
+require 'waiter.rb'
+require 'meal.rb'
+
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end #initialize
+
+  def self.all
+    @@all
+  end #self.all
+
 end
